9a99a4c3508adf8c870892bfaf12fda7007ad255
dsdhumale <d.sdhumale@gmail.com>
2022-09-29 23:38:33 +0530

    Added :  Print element of an array

---
+++ PrintElementOfAnArray.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz.Day3;
+
+public class PrintElementOfAnArray {
+	public static void main(String[] args) {
+		int elements[] = { 12, 13, 14, 15, 16 };
+		for (int element : elements) {
+			System.out.println("Array element are " + element);
+		}
+
+	}
+}
